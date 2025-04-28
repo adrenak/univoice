750bbb51ce2bf32a83f21c1f1a47e7515bf7e7b5
Vatsal Ambastha <ambastha.vatsal@gmail.com>
2025-04-28 14:03:09 +0530

    v4.4.0: EmptyAudioInput and mirror setup sample
    * EmptyAudioInput allows creating a session on a device that is not expected to or cannot send audio.
    * Added UniVoiceMirrorSetupSample sample, a quick drag and drop sample component that can be used to create a basic integration in a Mirror based project.

---
+++ Runtime/Impl/Inputs/EmptyAudioInput.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Adrenak.UniVoice {
+    /// <summary>
+    /// An audio input implementation that doesn't do anything.
+    /// Use this when the device doesn't have any input mode. 
+    /// This is especially useful when setting up the ClientSession
+    /// object on a dedicated server that likely isn't going to have 
+    /// and mic or other audio capture devices.
+    /// </summary>
+    public class EmptyAudioInput : IAudioInput {
+        public int Frequency => 1;
+
+        public int ChannelCount => 1;
+
+        public int SegmentRate => 1;
+
+        public event Action<AudioFrame> OnFrameReady;
+
+        public void Dispose() { }
+    }
+}
