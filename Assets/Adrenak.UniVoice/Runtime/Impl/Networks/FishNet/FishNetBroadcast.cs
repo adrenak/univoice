fdc3424180d8991c92b3e092b3edb50b6110c863
František Holubec <frantisek.holubec@gmail.com>
2025-08-14 18:48:08 +0200

    FishNet integration first draft

---
+++ Assets/Adrenak.UniVoice/Runtime/Impl/Networks/FishNet/FishNetBroadcast.cs
@@ -0,0 +1,22 @@
+﻿#if FISHNET
+using System;
+using FishNet.Broadcast;
+
+namespace Adrenak.UniVoice.Networks
+{
+    /// <summary>
+    /// The messages exchanged between the server and client. 
+    /// To see how the Mirror implementation of UniVoice uses this struct
+    /// find the references to the <see cref="data"/> object in the project.
+    /// The gist is, it uses BRW (https://www.github.com/adrenak/brw) to 
+    /// write and read data. The data always starts with a tag. All the tags
+    /// used for this UniVoice FishNet implementation are available in 
+    /// <see cref="FishNetBroadcastTags"/>
+    /// </summary>
+    [Serializable]
+    public struct FishNetBroadcast : IBroadcast
+    {
+        public byte[] data;
+    }
+}
+#endif
